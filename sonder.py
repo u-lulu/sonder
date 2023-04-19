655edfebb5be6d57fbc81bee2531994476d9dad0
ululu <mtmerlino@gmail.com>
2023-04-19 19:38:32 -0400

    fixed again

--- sonder.py
+++ sonder.py
@@ -320,7 +320,7 @@ async def d6(ctx, count: discord.Option(discord.SlashCommandOptionType.integer, 
 			sum += x
 			results += num_to_die[x] + " "
 			
-		results += ", ".join(numerical_results)
+		results += "\n[" + ", ".join(numerical_results) + "]"
 		if count > 1:
 			results += f"\n> **Total: {sum}**\n> Average: {sum/count}\n > Counts:"
 			for i in range(1,7):
