abcc140c832337c5cd4e62f4bfa4ce69a2559279
ululu <mtmerlino@gmail.com>
2023-05-02 12:15:53 -0400

    add combination tape support

--- sonder.py
+++ sonder.py
@@ -545,10 +545,20 @@ file.close()
 async def cassette(ctx):
 	log("/matrix cassette")
 	audio = rnd.choice(intelligence["cassettes"])
-    if not '[' in audio:
+    if audio == "[Combination tape, roll 1D6 tapes]":
+        tapes = ["[Combination tape, roll 1D6 tapes]"]
+        while "[Combination tape, roll 1D6 tapes]" in tapes:
+            tapes = rnd.sample(intelligence["cassettes"], rnd.randint(2,6))
+        for i in range(len(tapes)):
+            if not '[' in tapes[i]:
+                search = VideosSearch(tapes[i],limit=1)
+                result = search.result()['result'][0]
+                tapes[i] = f"[{tapes[i]}](<{result['link']}>)"
+        audio = "Combination tape:\n- " + "\n- ".join(tapes)
+    elif not '[' in audio:
         search = VideosSearch(audio,limit=1)
         result = search.result()['result'][0]
-        audio = f"[audio](<{result['link']}>)"
+        audio = f"[{audio}](<{result['link']}>)"
 	await ctx.respond(audio)
 
 gear_group = matrix_group.create_subgroup("gear", "Gear Intelligence Matrices")
