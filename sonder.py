8a66377a3240e6de282c4b1026d9ed7709124f97
ululu <mtmerlino@gmail.com>
2023-06-22 13:23:30 -0400

    fully ephemeral command

--- sonder.py
+++ sonder.py
@@ -564,13 +564,13 @@ async def active_character(ctx, show_all: discord.Option(bool, "If TRUE, lists a
 			message = f"Your characters are active in the following {len(your_actives)} channels:"
 			for channel in your_actives:
 				message += f"\n- <#{channel}> -> {your_actives[channel].upper()}"
-			await ctx.respond(message)
+			await ctx.respond(message,ephemeral=True)
 		else:
 			await ctx.respond(f"You do not have active characters in any channels.",ephemeral=True)
 	else:
 		codename = get_active_codename(ctx)
 		if codename != None:
-			await ctx.respond(f"Your active character in this channel is **{codename.upper()}**.")
+			await ctx.respond(f"Your active character in this channel is **{codename.upper()}**.",ephemeral=True)
 		else:
 			await ctx.respond(f"You do not have an active character in this channel.",ephemeral=True)
 
