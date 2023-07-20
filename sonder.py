9b74abbab7381581348620d494dd2c54b147b6ed
ululu <mtmerlino@gmail.com>
2023-07-20 15:49:29 -0400

    logging stuff

--- sonder.py
+++ sonder.py
@@ -1643,7 +1643,7 @@ async def remove_item(ctx,
 
 @bot.command(description="Spend a War Die from your active character")
 async def war_die(ctx, explode: discord.Option(bool, "If TRUE, this roll follows the 'Exploding WAR DICE' optional rule.", required=False, default=False)):
-	log(f"/war_die")
+	log(f"/war_die{' explode' if explode else ''}")
 	character = get_active_char_object(ctx)
 	if character == None:
 		await ctx.respond("You do not have an active character in this channel. Select one with `/switch`.",ephemeral=True)
@@ -1696,6 +1696,7 @@ async def war_die(ctx, explode: discord.Option(bool, "If TRUE, this roll follows
 							if interaction.user.id == ctx.author.id:
 								self.disable_all_items()
 								await interaction.response.edit_message(view=self)
+								log("Sending Explode response")
 								results = [6]
 								while results[-1] == 6:
 									results.append(d6())
@@ -1711,14 +1712,17 @@ async def war_die(ctx, explode: discord.Option(bool, "If TRUE, this roll follows
 									message += f"\n- Total: **{0 if 1 in results else sum(results)}**"
 								await ctx.respond(message)
 							else:
+								log("Denying invalid Explode response")
 								await interaction.response.send_message("This is not your War Die roll.",ephemeral=True)
 						@discord.ui.button(label="Keep the " + str(nonsix), style=discord.ButtonStyle.blurple, emoji="🎲")
 						async def safety_callback(self, button, interaction):
 							if interaction.user.id == ctx.author.id:
+								log("Sending safe response")
 								self.disable_all_items()
 								await interaction.response.edit_message(view=self)
 								await ctx.respond(f"**{codename.upper()}** keeps the **{num_to_die[nonsix]} ({nonsix})**.{' The final result is **zero**.' if nonsix == 1 else ''}")
 							else:
+								log("Denying invalid safe response")
 								await interaction.response.send_message("This is not your War Die roll.",ephemeral=True)
 					await ctx.respond(f"**{codename.upper()}** spends a **Fated** War Die. **They must choose:**\n- **{num_to_die[6]} (6💥)**\n- **{num_to_die[nonsix]} ({nonsix}{' - __reduces to 0__!' if nonsix == 1 else ''})**\nThey have {remaining} War Di{'e' if remaining == 1 else 'ce'} left.",view=DiePicker())
 				else:
