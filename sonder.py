2e310d90320229a3597db297197dfd1ad10a5471
ululu <mtmerlino@gmail.com>
2023-12-01 16:30:12 -0500

    prevent /credits from opening a file every call

--- sonder.py
+++ sonder.py
@@ -518,9 +518,16 @@ async def invite(ctx):
 async def server(ctx):
 	await ctx.respond("https://discord.gg/VeedQmQc7k",ephemeral=True)
 
+credits_file = open('credits.md')
+credits_string = credits_file.read()
+credits_file.close()
+
+log(f"Got credits text ({len(credits_string)} characters long)")
+
 @bot.command(description="Lists people responsible for this bot's creation")
 async def credits(ctx):
-	await ctx.respond(open('credits.md').read(),ephemeral=True)
+	await ctx.defer(ephemeral=True)
+	await ctx.respond(credits_string,ephemeral=True)
 
 @bot.command(description="Check to see if you have an active membership")
 async def membership(ctx):
