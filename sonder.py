098949406b0578cec7c2cd9e9d1fd428f7dba128
ululu <mtmerlino@gmail.com>
2024-12-04 01:27:39 -0500

    remove message log thing (it didnt work because im stupid. oops)

--- sonder.py
+++ sonder.py
@@ -510,7 +510,6 @@ else:
 
 log("Checking to see if character data needs to be updated...")
 changed = False
-volatile_changes = {}
 for player in character_data:
 	if 'traits' not in character_data[player]:
 		character_data[player]['traits'] = {}
@@ -559,10 +558,6 @@ for player in character_data:
 					vtrait = select_random_novel_trait(character_data[player]['chars'][char])
 					character_data[player]['chars'][char]['special']['volatile_trait'] = vtrait
 					apply_stat_change(character_data[player]['chars'][char],vtrait['Stat'])
-					if player not in volatile_changes:
-						volatile_changes[player] = [(char.upper(),vtrait['Name'],vtrait['Number'])]
-					else:
-						volatile_changes[player].append((char.upper(),vtrait['Name'],vtrait['Number']))
 					break
 
 if changed:
@@ -642,22 +637,6 @@ async def on_ready():
 		report_character_count += len(character_data[player]['chars'])
 		report_trait_count += len(character_data[player]['traits'])
 	log(f"Currently tracking {report_player_count} players, {report_character_count} characters, and {report_trait_count} custom traits.")
-
-	global volatile_changes
-	for player in volatile_changes:
-		if len(volatile_changes[player]) > 0:
-			try:
-				user = await bot.fetch_user(int(player))
-				msg = "This is a one-time message to notify you of changes to one or more of your characters."
-				msg += "\nThe VOLATILE trait has been updated with functionality. As such, characters you own with VOLATILE have been assigned a new auxillary trait."
-				for v in volatile_changes[player]:
-					msg += f"\n- {v[0]} has been assigned {v[1]} ({v[2]})."
-				msg += replace_commands_with_mentions("\nIf you would like to change this trait assignment, you can do so with the `/volatile` command.")
-				await user.send(msg,silent=True)
-			except Exception as e:
-				raise e
-	
-	del volatile_changes
 
 	asyncio.create_task(backup_generation_loop())
 
